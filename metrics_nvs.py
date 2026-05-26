3891d91b9530954c5ed0e23fb66ef1a8a65bdc2e
haebeom <zang09@naver.com>
2026-05-26 20:54:37 +0900

    update license

--- metrics_nvs.py
+++ metrics_nvs.py
@@ -1,17 +1,12 @@
 #
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# Modifications Copyright (C) 2026, SNU
+# Copyright (C) 2026, SNU
 # SNU VGI lab
-# Modified for TLC-Calib: run NVS evaluation from calibrated
-# LiDAR-camera poses and store results inside the calibration output.
+# All rights reserved.
 #
 # This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
-# For inquiries contact george.drettakis@inria.fr and haebeom.jung@snu.ac.kr
+# For inquiries contact haebeom.jung@snu.ac.kr
 #
 
 import json
--- metrics_pose.py
+++ metrics_pose.py
@@ -1,17 +1,12 @@
 #
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# Modifications Copyright (C) 2026, SNU
+# Copyright (C) 2026, SNU
 # SNU VGI lab
-# Modified for TLC-Calib: added rig and camera pose error
-# evaluation for LiDAR-camera calibration.
+# All rights reserved.
 #
 # This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
-# For inquiries contact george.drettakis@inria.fr and haebeom.jung@snu.ac.kr
+# For inquiries contact haebeom.jung@snu.ac.kr
 #
 
 import os
